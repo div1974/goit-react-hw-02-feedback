1b9327d1fc360efe02a65e92cf9c7f4380a9c538
div1974 <div@ukr.net>
2020-08-30 00:17:42 +0300

    Updates

---
+++ precache-manifest.d5373e013559f48122381eb96e4d3ca4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0704c46d29c78851c1ef2504c57af44b",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "eb6564e68ccc1cbe4d00",
+    "url": "/goit-react-hw-02-feedback/static/css/main.9aa60280.chunk.css"
+  },
+  {
+    "revision": "31e544bab9fff2939c15",
+    "url": "/goit-react-hw-02-feedback/static/js/2.22a4be83.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.22a4be83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb6564e68ccc1cbe4d00",
+    "url": "/goit-react-hw-02-feedback/static/js/main.77d61398.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
